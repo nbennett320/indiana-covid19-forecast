5a33fd679d786c0036bfe5b1fc295e67e0d3028b
Noah Bennett <nbennett.320@gmail.com>
2020-10-28 13:55:25 -0400

    rm commented code

--- scripts/format_time_series.py
+++ scripts/format_time_series.py
@@ -14,22 +14,8 @@ def get_indiana_confirmed():
   fips_df = data_time_series_cases['FIPS']
   lats_df = data_time_series_cases['Lat']
   longs_df = data_time_series_cases['Long_']
-  # result_df = pd.DataFrame([
-  #   counties_df,
-  #   state_df,
-  #   country_df,
-  #   combined_keys_df,
-  #   fips_df,
-  #   lats_df,
-  #   longs_df,
-  # ]).T
-
   cases_df = data_time_series_cases.iloc[:,11:]
   deaths_df = data_time_series_deaths.iloc[:,12:]
-  print("counties")
-  print(counties_df)
-  print("state")
-  print(len(cases_df.columns))
   result_df = pd.DataFrame(columns=[
     'date', 
     'county', 
@@ -42,8 +28,8 @@ def get_indiana_confirmed():
     'cases', 
     'deaths'
   ])
-  print(lats_df)
-  print(cases_df[cases_df.columns[93]])
+
+  # populate data frame rows
   k = 0
   for i in cases_df.columns:
     for j in range(0, len(counties_df)):
@@ -60,42 +46,10 @@ def get_indiana_confirmed():
         'cases': cases_df[i][j], 
         'deaths': deaths_df[i][j]
       }
-      # print('new row')
-      # print(new_row)
       result_df.loc[k] = new_row
       k += 1
-      # print(result_df)
-      # print(k)
   print('result')
   print(result_df)
-
-
-
-  # {
-  #   'date', 
-  #   'county', 
-  #   'state', 
-  #   'country', 
-  #   'full_key', 
-  #   'lat', 
-  #   'long', 
-  #   'fips', 
-  #   'cases', 
-  #   'deaths'
-  # }
-  # select cases and deaths per date
-  # dates_df = data_time_series_cases.iloc[:,11:]
-  # deaths_df = data_time_series_deaths.iloc[:,12:]
-
-  # # append cases per day
-  # for col in dates_df.columns:
-  #   key = 'cases_' + col
-  #   result_df[key] = dates_df[col]
-
-  # # append deaths per day
-  # for col in deaths_df.columns:
-  #   key = 'deaths_' + col
-  #   result_df[key] = deaths_df[col]
   result_df.to_csv(r'./data/indiana_county_level_time_series_data.csv')
 
 # main
